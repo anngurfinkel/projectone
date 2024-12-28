8f854d92dc9d5eeddaffb22bb27d8c0743e1872c
Ann Gurfinkel <ann.gurfinkel@MacBook-Pro-Ann.local>
2024-12-28 16:25:32 +0200

    Add file homework_5.2.py to lesson_5.2

--- lesson_5.2/homework_5.2.py
+++ lesson_5.2/homework_5.2.py
@@ -34,8 +34,48 @@ people_records[1], people_records[5] = people_records[5], people_records[1]
 print(people_records)
 
 # 3
+#check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30.
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+people_records.insert(0, ('Anna', 'Hurfinkel', 28, 'QA Specialist', 'Kyiv'))
+print(people_records)
 
 
+people_records[1], people_records[5] = people_records[5], people_records[1]
 
+print(people_records)
 
+#indexes 6, 10, 13
+
+people_records_2 = (people_records[6],people_records[10],people_records[13])
+print(people_records_2)
+
+#age >=30
+
+def criteria(age):
+    return age[2] >=30
+    
+
+def people(records):
+        result = list(filter(criteria, records))
+        return result
+
+print(people(people_records_2))
 
